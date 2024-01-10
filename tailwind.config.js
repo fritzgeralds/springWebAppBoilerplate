56ba8c10c98c64cdfcce6aef8c403ee0fa92d26b
Ludwig Dischner <fritz@dischner.me>
2024-01-10 12:54:18 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './src/main/resources/templates/**/*.html',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [
+
+  ],
+}
+
